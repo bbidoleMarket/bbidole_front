ad376f2bc1fe8f7cfef113ae6713c5d947559041
성현주 <vivivim@github.com>
2025-06-18 17:13:16 +0900

    Initial: 기본 라우터 설정

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -2,25 +2,41 @@
   <header
     class="fixed top-0 right-0 left-0 flex w-full h-[80px] bg-[#FFFCEF] shadow-md p-4"
   >
-    <img
-      src="../assets/bbidole.svg"
-      alt="삐돌이 마켓 로고"
-      class="h-40 fixed top-0"
-    />
+    <!-- 로고 겸 홈으로 버튼 -->
+    <BaseButton class="bg-transparent hover:bg-transparent" @click="goHome">
+      <img
+        src="../assets/bbidole.svg"
+        alt="삐돌이 마켓 로고"
+        class="h-40 fixed top-0"
+      />
+    </BaseButton>
     <div class="flex w-full h-full justify-center items-center">
-      <h1 class="text-3xl font-bold text-[#2E383A]">삐돌이 마켓</h1>
+      <h1 class="text-3xl font-bold text-[#2E383A] ml-auto">삐돌이 마켓</h1>
+      <!-- 로그인 버튼 -->
+      <BaseButton
+        class="ml-auto mr-4 w-20 p-4 bg-[#43A8A5] text-lg text-white font-bold flex items-center justify-center"
+        @click="goLogin"
+      >
+        로그인
+      </BaseButton>
+      <!-- 햄버거 메뉴 버튼 -->
+      <BaseButton class="p-4 bg-[#43A8A5]">
+        <img src="../assets/hamburger.svg" alt="햄버거 메뉴" class="h-7" />
+      </BaseButton>
     </div>
-    <BaseButton
-      class="mr-4 w-20 p-4 bg-[#43A8A5] text-white font-bold flex items-center justify-center"
-    >
-      로그인
-    </BaseButton>
-    <BaseButton class="p-4 bg-[#43A8A5] flex items-center justify-center">
-      <img src="../assets/hamburger.svg" alt="햄버거 메뉴" class="h-7" />
-    </BaseButton>
   </header>
 </template>
 
 <script setup>
 import BaseButton from "../components/base/BaseButton.vue";
+import router from "../router";
+
+const goHome = () => {
+  // 홈으로 이동하는 함수
+  router.push("/");
+};
+const goLogin = () => {
+  // 로그인 페이지로 이동하는 함수
+  router.push("/login");
+};
 </script>
