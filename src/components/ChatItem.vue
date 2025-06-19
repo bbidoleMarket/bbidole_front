f7b0cf709874fb98c053e4ef9ccc34d68b05dd47
성현주 <vivivim@github.com>
2025-06-19 18:36:55 +0900

    Feat: 내 채팅방 목록 페이지 기본 틀만^^

---
+++ src/components/ChatItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="border p-4 mb-4 bg-white rounded-lg shadow cursor-pointer hover:bg-gray-100"
+  >
+    <h3>{{ chat.title }}</h3>
+    <!-- 상대방 닉네임 표시 -->
+    <p>{{ chat.seller == myname ? chat.buyer : chat.seller }}</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  chat: Object,
+});
+const myname = "user1"; // 현재 사용자 이름, 실제로는 로그인 정보에서 가져와야 함
+</script>
