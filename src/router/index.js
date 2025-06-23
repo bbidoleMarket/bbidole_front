ec43cfed3e578f1d3b3c93d3908f521063f74ee8
성현주 <vivivim@github.com>
2025-06-23 12:35:30 +0900

    Feat: 채팅방 입장 시 입장한 채팅방 페이지와 메시지 컴포넌트들 채팅방으로 이동 라우트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MeChats from "../pages/MeChats.vue";
+import ChatRoom from "../pages/ChatRoom.vue";
 
-const routes = [{ path: "/me/chats", component: MeChats }];
+const routes = [
+  { path: "/me/chats", component: MeChats },
+  { path: "/chat/:chatId", component: ChatRoom },
+];
 
 const router = createRouter({
   history: createWebHistory(),
