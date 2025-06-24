746ca830a5fe3c5e69b2c81baa8cb56efb813ffa
parkjiwon <ji11won@naver.com>
2025-06-24 10:51:35 +0900

    Feat: 전역 에러 핸들러 추가

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import router from "./router";
 
 const app = createApp(App);
 const pinia = createPinia();
-app.use(pinia);
-app.use(router);
-app.mount("#app");
+app.use(pinia); // Pinia 등록
+app.use(router); // Vue Router 등록
+app.mount("#app"); // index.html의 app에 마운트
+
+// 전역 오류 핸들러 설정 -> Vue 3 방식
+app.config.errorHandler = (err, vm, info) => {
+  console.error("🐛 전역 오류 발생:", err);
+  console.warn("컴포넌트 인스턴스:", vm);
+  console.info("오류 정보:", info);
+};
