be1e3ce0da9d135b70bf838585f795ef4e4a8ac7
parkjiwon <ji11won@naver.com>
2025-06-24 11:45:33 +0900

    Feat: path 설정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      // "@": path.resolve(__dirname, ".src"),
+      "@": path.resolve(__dirname, "./src"), // 파일 경로 설정
     },
   },
 });
