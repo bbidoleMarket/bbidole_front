ca27fb149d4999a5b87ccd385e145ace30082702
parkjiwon <ji11won@naver.com>
2025-06-24 21:37:43 +0900

    Feat: ReviewPage 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MeChats from "@/pages/MeChats.vue";
-import MyPage from "@/pages/MyPage.vue";
 
 const routes = [
   { path: "/me/chats", component: MeChats },
-  { path: "/me", component: MyPage },
+  {
+    path: "/me",
+    name: "MyPage",
+    component: () => import("@/pages/MyPage.vue"),
+    meta: { requiresAuth: true },
+  },
   {
     path: "/post/:postId",
     name: "PostDetail",
     component: () => import("@/pages/PostDetail.vue"),
+  },
+  {
+    path: "/user/:userId",
+    name: "UserDetail",
+    component: () => import("@/pages/MyPage.vue"),
+  },
+  {
+    path: "/reviews/:userId",
+    name: "UserReviews",
+    component: () => import("@/pages/ReviewPage.vue"),
   },
 ];
 
