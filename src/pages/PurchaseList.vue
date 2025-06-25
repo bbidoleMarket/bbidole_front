a9299e6da94841af30290aa16b350cb721f2804a
kkksohee <thgml0716030@naver.com>
2025-06-25 10:51:42 +0900

    Feat: page추가
    구매목록

---
+++ src/pages/PurchaseList.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--가장 밖-->
+  <div class="flex flex-col justify-center items-center min-h-screen">
+    <!-- 구매 목록-->
+    <div>
+      <h2 class="font-jua">구매 목록</h2>
+    </div>
+    <!-- 카테고리-->
+    <div></div>
+    <!--리스트 -->
+    <div></div>
+  </div>
+</template>
+
+<script setup></script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 //import MeChats from "../pages/MeChats.vue";
 
-import mypage from "../pages/mypage.vue";
 import Mypage from "../pages/mypage.vue";
+import PurchaseList from "../pages/PurchaseList.vue";
 
-const routes = [{ path: "/mypage", component: Mypage }];
+const routes = [
+  { path: "/mypage", component: Mypage }, //회원정보 수정
+  { path: "/purchaselist", component: PurchaseList }, //구매내역
+];
 
 const router = createRouter({
   history: createWebHistory(),
