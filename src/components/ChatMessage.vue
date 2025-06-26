e76d945ee1c34d2562af0a613eac7a5138ec8824
성현주 <vivivim@github.com>
2025-06-26 12:06:37 +0900

    Fix: 챗메시지 그릴 때 상대방 구분 아까 잘못 고친거 다시 되돌림 왜저래...

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -2,7 +2,7 @@
   <div
     class="max-w-[50%] rounded-[12px] break-words p-2 font-noto mx-4 mt-4 text-xs lg:text-lg"
     :class="
-      message.senderId === Number(message.othersId)
+      message.senderId === Number(othersId)
         ? 'mr-auto bg-[#EAF9EA]'
         : 'ml-auto bg-[#B0DEDE]'
     "
@@ -12,9 +12,7 @@
   <p
     class="text-xs text-[#555] mb-4"
     :class="
-      message.senderId === Number(message.othersId)
-        ? 'mr-auto ml-6'
-        : 'mr-6 ml-auto'
+      message.senderId === Number(othersId) ? 'mr-auto ml-6' : 'mr-6 ml-auto'
     "
   >
     {{ formatDateTime(message.sendAt) }}
@@ -29,6 +27,8 @@ const props = defineProps({
   },
   othersId: [Number, String],
 });
+
+console.log(props.message);
 
 function formatDateTime(dateTime) {
   return dateTime.toString().slice(0, 16).replace("T", " ");
