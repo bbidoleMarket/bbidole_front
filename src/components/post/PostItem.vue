1c9dbc6301b50bf8474257ea3360d7405522db15
parkjiwon <ji11won@naver.com>
2025-06-27 09:29:46 +0900

    Feat: 게시물 정보 component 추가

---
+++ src/components/post/PostItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="flex items-center gap-4 border-t py-4 px-2 rounded-xl hover:bg-[#319B9A] cursor-pointer overflow-x-clip"
+    @click="
+      () => {
+        goPostDetail(post.postId);
+      }
+    "
+  >
+    <div class="w-16 h-16 rounded-full bg-gray-200 overflow-hidden shrink-0">
+      <img
+        :src="post.imageUrl || '@/assets/icon_bbidole.svg'"
+        alt="product"
+        class="object-cover w-full h-full"
+      />
+    </div>
+    <div class="">
+      <p class="font-hahmlet font-bold text-gray-900 truncate">
+        {{ post.title || "물품 제목" }}
+      </p>
+      <p class="font-hahmlet text-gray-700">가격 : {{ post.price || 0 }}원</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+  },
+});
+
+const goPostDetail = (postId) => {
+  router.push(`/post/${postId}`);
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/post/PostList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- <h2 class="text-3xl font-bold font-jua text-center mb-6">
+        판매중인 물품
+      </h2> -->
+    <div class="relative mb-6">
+      <!-- 중앙 정렬 제목 -->
+      <h2 class="text-3xl font-bold font-jua text-center">{{ title }}</h2>
+
+      <!-- 오른쪽 상단 버튼 -->
+      <BaseButton
+        class="absolute right-0 top-1/2 -translate-y-1/2 text-xs sm:text-sm lg:text-lg lg:px-4 lg:py-2 md:hidden block"
+      >
+        더보기
+      </BaseButton>
+    </div>
+
+    <div class="space-y-6 overflow-y-auto max-h-80 hidden-scroll-bar">
+      <!-- 물품 카드 반복 -->
+      <PostItem
+        v-for="post in postList"
+        :key="post.postId"
+        :post="post"
+      ></PostItem>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import BaseButton from "@/components/base/BaseButton.vue";
+import PostItem from "@/components/post/PostItem.vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: "리스트",
+  },
+  postList: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.background {
+  background-color: #fffff6;
+}
+.hidden-scroll-bar {
+  -ms-overflow-style: none; /* IE/Edge */
+  scrollbar-width: none; /* Firefox */
+}
+.hidden-scroll-bar::-webkit-scrollbar {
+  display: none; /* Chrome, Safari */
+}
+</style>
