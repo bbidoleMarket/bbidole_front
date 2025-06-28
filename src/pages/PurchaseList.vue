27e188ca9f7d57d7cd4127e022b9fce6fd238917
kkksohee <thgml0716030@naver.com>
2025-06-29 04:08:43 +0900

    Feat: 무한스크롤 추가

--- src/pages/PurchaseList.vue
+++ src/pages/PurchaseList.vue
@@ -7,7 +7,52 @@
     </div>
 
     <!--리스트 -->
-    <div>
+    <!--모바일 화면에서 무한 스크롤-->
+    <div
+      v-if="isMobile"
+      ref="scrollContainer"
+      @scroll="handleScroll"
+      class="border h-[60vh] overflow-auto"
+    >
+      <ul
+        class="w-screen space-y-4 flex flex-col justify-center items-center gap-2"
+      >
+        <li
+          v-for="purchase in purchaseList"
+          :key="purchase.purchaseId"
+          @click="goDetail(purchase.purchaseId)"
+          class="hover:bg-gray-100 p-2 border-gray-300 shadow w-full max-w-md md:max-w-wl lg:max-w-3xl max-auto transition-all duration-300 flex flex-row gap-7 rounded-md"
+        >
+          <!-- 게시글 이미지-->
+          <div
+            class="w-20 h-20 flex justify-center items-center overflow-hidden rounded-md"
+          >
+            <img
+              src="/image/stanley.jpg"
+              alt="게시글 사진"
+              class="object-contain"
+            />
+          </div>
+
+          <div class="flex flex-col gap-2 mt-2">
+            <!-- 게시글 제목-->
+            <div>
+              <h2 class="font-hahmlet">{{ purchase.title }}</h2>
+            </div>
+            <!-- 게시글 가격-->
+            <div>
+              <p class="font-hahmlet">{{ purchase.price }}원</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <!-- 무한스크롤 대기중-->
+      <div v-if="isLoading" class="text-center">
+        <i class="fa-solid fa-spinner"></i>
+      </div>
+    </div>
+    <!--페이지네이션-->
+    <div v-if="!isMobile" class="flex flex-col justify-center items-center">
       <ul
         class="w-screen space-y-4 flex flex-col justify-center items-center gap-2"
       >
@@ -40,36 +85,36 @@
           </div>
         </li>
       </ul>
-    </div>
-    <!--페이지네이션-->
-    <div>
-      <button
-        @click="prePage"
-        :disableed="currentPage == 1"
-        class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
-      >
-        <
-      </button>
-      <button
-        v-for="page in totalPage"
-        :key="page"
-        @click="goTopage(page)"
-        :class="[
-          'p-3 py-1 rounded-border',
-          currentPage == page - 1
-            ? 'bg-[#45A8A6] text-white'
-            : 'bg-gray-300 hover:bg-gray-200',
-        ]"
-      >
-        {{ page }}
-      </button>
-      <button
-        @click="nextPage"
-        :disableed="currentPage == totalPage"
-        class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
-      >
+      <!--페이지네이션-->
+      <div>
+        <button
+          @click="prePage"
+          :disableed="currentPage == 1"
+          class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
         >
-      </button>
+          <
+        </button>
+        <button
+          v-for="page in totalPage"
+          :key="page"
+          @click="goTopage(page)"
+          :class="[
+            'p-3 py-1 rounded-border',
+            currentPage == page - 1
+              ? 'bg-[#45A8A6] text-white'
+              : 'bg-gray-300 hover:bg-gray-200',
+          ]"
+        >
+          {{ page }}
+        </button>
+        <button
+          @click="nextPage"
+          :disableed="currentPage == totalPage"
+          class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
+        >
+          >
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -79,6 +124,8 @@ import { ref, onMounted } from "vue";
 import { UseTransactionListAPi } from "@/api/userTransaction";
 import { useRouter } from "vue-router";
 
+import { onUnmounted, watch } from "vue";
+
 const router = useRouter();
 
 const { purchaseList: fetchPurchaseList } = UseTransactionListAPi();
@@ -86,24 +133,81 @@ const purchaseList = ref([]);
 const currentPage = ref(0);
 const pageSize = 5;
 const totalPage = ref(0);
+const lastPage = ref(false);
 
-// onMounted(() => {
-//   fetchPageData();
-// });
-//나중에 변경
+//반응형 화면 감지
+const isMobile = ref(window.innerWidth <= 393);
+const handleResize = () => {
+  isMobile.value = window.innerWidth <= 393;
+};
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+  //scrollContainer.value?.addEventListener("scroll", handleScroll);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
 onMounted(async () => {
   const res = await fetchPageData();
   console.log(res);
   //purchase.value = res.data;
   //await fetchPageData();
 });
+//화면이 바뀌면 게시글 리스트 처음 부터 불러옴
+watch(isMobile, async (mobile) => {
+  console.log("isMobile: ", isMobile.value);
+  currentPage.value = 0;
+  purchaseList.value = [];
+  lastPage.value = 0;
+  await fetchPageData();
+});
+
+//무한 스크롤
+const isLoading = ref(false);
+const scrollContainer = ref(null);
+//스크롤 이벤트 발생 시
+const handleScroll = () => {
+  console.log("dkssud");
+  if (isLoading.value) {
+    console.log("isloading is true");
+    return;
+  }
+
+  const el = scrollContainer.value;
+  if (el.scrollTop + el.clientHeight >= el.scrollHeight - 10) {
+    //console.log("scroll !!! 걸림 ");
+    fetchMoreData();
+  }
+};
+
 const fetchPageData = async () => {
-  const res = await fetchPurchaseList(currentPage.value, pageSize);
-  console.log(res);
-  //게시글 목록
+  isLoading.value = true;
+  const res = await fetchPurchaseList(userId, currentPage.value, pageSize);
   purchaseList.value = res.data.data.content;
-  //총 페이지 수
   totalPage.value = res.data.data.totalPages;
+  isLoading.value = false;
+};
+
+const fetchMoreData = async () => {
+  //로딩중이거나 마지막 페이지라면 return
+  if (isLoading.value || lastPage.value) {
+    console.log(
+      "isLoading.value ,lastPage.value ",
+      isLoading.value,
+      " ",
+      lastPage.value
+    );
+    return;
+  }
+  isLoading.value = true;
+  currentPage.value++;
+
+  const res = await fetchPurchaseList(userId, currentPage.value, pageSize);
+  purchaseList.value = [...purchaseList.value, ...res.data.data.content];
+  lastPage.value = res.data.data.last; //마지막 페이지 여부
+  isLoading.value = false;
+  console.log("서버에서 데이터 받아옴", res);
 };
 
 const prePage = () => {
--- src/pages/SalesList.vue
+++ src/pages/SalesList.vue
@@ -19,9 +19,13 @@
         </select>
       </div>
     </div>
-
-    <!--리스트 -->
-    <div>
+    <!--모바일 화면에서 무한 스크롤-->
+    <div
+      v-if="isMobile"
+      ref="scrollContainer"
+      @scroll="handleScroll"
+      class="border h-[60vh] overflow-auto"
+    >
       <ul
         class="w-screen space-y-4 flex flex-col justify-center items-center gap-2"
       >
@@ -56,34 +60,69 @@
       </ul>
     </div>
     <!--페이지네이션-->
-    <div>
-      <button
-        @click="prePage"
-        :disableed="currentPage == 1"
-        class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
+    <div v-if="!isMobile" class="flex flex-col justify-center items-center">
+      <ul
+        class="w-screen space-y-4 flex flex-col justify-center items-center gap-2"
       >
-        <
-      </button>
-      <button
-        v-for="page in totalPage"
-        :key="page"
-        @click="goTopage(page)"
-        :class="[
-          'p-3 py-1 rounded-border',
-          currentPage == page - 1
-            ? 'bg-[#45A8A6] text-white'
-            : 'bg-gray-300 hover:bg-gray-200',
-        ]"
-      >
-        {{ page }}
-      </button>
-      <button
-        @click="nextPage"
-        :disableed="currentPage == totalPage"
-        class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
-      >
+        <li
+          v-for="sales in salesList"
+          :key="sales.salesId"
+          @click="goDetail(sales.postId)"
+          class="hover:bg-gray-100 p-2 border-gray-300 shadow w-full max-w-md md:max-w-wl lg:max-w-3xl max-auto transition-all duration-200 flex flex-row gap-7 rounded-md"
         >
-      </button>
+          <!-- 게시글 이미지-->
+          <div
+            class="w-20 h-20 flex justify-center items-center overflow-hidden rounded-md"
+          >
+            <img
+              :src="sales.imageUrl"
+              alt="게시글 사진"
+              class="object-contain"
+            />
+          </div>
+
+          <div class="flex flex-col gap-2 mt-2">
+            <!-- 게시글 제목-->
+            <div>
+              <h2 class="font-hahmlet">{{ sales.title }}</h2>
+            </div>
+            <!-- 게시글 가격-->
+            <div>
+              <p class="font-hahmlet">{{ sales.price }}원</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <!--페이지네이션-->
+      <div>
+        <button
+          @click="prePage"
+          :disableed="currentPage == 1"
+          class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
+        >
+          <
+        </button>
+        <button
+          v-for="page in totalPage"
+          :key="page"
+          @click="goTopage(page)"
+          :class="[
+            'p-3 py-1 rounded-border',
+            currentPage == page - 1
+              ? 'bg-[#45A8A6] text-white'
+              : 'bg-gray-300 hover:bg-gray-200',
+          ]"
+        >
+          {{ page }}
+        </button>
+        <button
+          @click="nextPage"
+          :disableed="currentPage == totalPage"
+          class="px-3 py-1 rounded-sm border border-grey-300 hover:bg-opacity-90 disabled:opacity-50"
+        >
+          >
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -92,6 +131,7 @@
 import { ref, onMounted, watch } from "vue";
 import { UseTransactionListAPi } from "@/api/userTransaction";
 import { useRouter } from "vue-router";
+import { watch, onUnmounted } from "vue";
 
 const router = useRouter();
 const currentPage = ref(0);
@@ -99,29 +139,71 @@ const pageSize = 5;
 const totalPage = ref(0);
 const { salesListLatest, salesListOnSales, salesListCompletedSales } =
   UseTransactionListAPi();
-//const salesList = ref([]);
 const selectedCategory = ref("all");
 const salesList = ref([]);
+const isLoading = ref(false);
+const scrollContainer = ref(null);
+const lastPage = ref(false);
+
+//화면 사이즈 지정
+const isMobile = ref(window.innerWidth <= 393);
+const handleResize = () => {
+  isMobile.value = window.innerWidth <= 393;
+};
+//화면 사이즈 확인
+onMounted(() => {
+  //console.log("sentinel:", sentinel.value);
+  window.addEventListener("resize", handleResize);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
 onMounted(async () => {
   await fetchPageData();
 });
-//나중에 변경
-// onMounted(async()=>{
-//   const res = await salesListLatest();
-//   salesList.value= res.data;
-// })
 
+//스크롤 이벤트 발생 시
+const handleScroll = () => {
+  console.log("dkssud");
+  if (isLoading.value) {
+    console.log("isloading is true");
+    return;
+  }
+
+  const el = scrollContainer.value;
+  if (el.scrollTop + el.clientHeight >= el.scrollHeight - 10) {
+    //console.log("scroll !!! ");
+    fetchMoreData();
+  }
+};
+//카테고리 변경
 watch(selectedCategory, async () => {
   currentPage.value = 0;
+  salesList.value = [];
+  lastPage.value = 0;
   await fetchPageData();
 });
 
-const fetchPageData = async () => {
-  const fetcher = {
+watch(isMobile, async (mobile) => {
+  console.log("isMobile: ", isMobile.value);
+  currentPage.value = 0;
+  salesList.value = [];
+  lastPage.value = 0;
+  await fetchPageData();
+});
+//카테고리 기준으로 올바른 함수반환
+function getFetcher() {
+  return {
     all: salesListLatest,
     onsales: salesListOnSales,
     completed: salesListCompletedSales,
   }[selectedCategory.value];
+}
+
+const fetchPageData = async () => {
+  isLoading.value = true;
+  const fetcher = getFetcher();
 
   if (!fetcher) return;
   const res = await fetcher(currentPage.value, pageSize);
@@ -129,7 +211,29 @@ const fetchPageData = async () => {
   salesList.value = res.data.data.content;
   //총 페이지 수
   totalPage.value = res.data.data.totalPages;
+  isLoading.value = false;
   console.log(res);
+};
+
+const fetchMoreData = async () => {
+  //로딩중이거나 마지막 페이지라면 return
+  if (isLoading.value || lastPage.value) {
+    console.log(
+      "isLoading.value ,lastPage.value ",
+      isLoading.value,
+      " ",
+      lastPage.value
+    );
+    return;
+  }
+  isLoading.value = true;
+  currentPage.value++;
+  const fetcher = getFetcher();
+  const res = await fetcher(userId, currentPage.value, pageSize);
+  salesList.value = [...salesList.value, ...res.data.data.content];
+  lastPage.value = res.data.data.last; //마지막 페이지 여부
+  isLoading.value = false;
+  console.log("서버에서 데이터 받아옴", res);
 };
 
 const prePage = () => {
