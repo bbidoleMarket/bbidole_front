ebfe63f91541d6f442d385b69dbd745f86c7478d
parkjiwon <ji11won@naver.com>
2025-07-06 21:21:22 +0900

    Feat: 신고

--- src/api/report.js
+++ src/api/report.js
@@ -1,46 +1,55 @@
 import axios from "./axiosInstance";
 
 export const useReportApi = () => {
-    const userReportList = async () => {
-        return await axios.get("/api/admin/report/userlist");
-    };
-    const userReportDetail = async (reportId) => {
-        return await axios.get(`/api/admin/report/userlist-detail/${reportId}`);
-    };
-    const updateUserReport = async (reportId, newStatus) => {
-        return await axios.put(
-            `/api/admin/report/user-status/${reportId}/status`,
-            null,
-            {
-                params: {
-                    status: newStatus,
-                },
-            }
-        );
-    };
-    const postReportList = async () => {
-        return await axios.get("/api/admin/report/postlist");
-    };
-    const postReportDetail = async (reportId) => {
-        return await axios.get(`/api/admin/report/postlist-detail/${reportId}`);
-    };
-    const updatePostReport = async (reportId, newStatus) => {
-        return await axios.put(
-            `/api/admin/report/post-status/${reportId}/status`,
-            null,
-            {
-                params: {
-                    status: newStatus,
-                },
-            }
-        );
-    };
-    return {
-        userReportList,
-        userReportDetail,
-        updateUserReport,
-        postReportList,
-        postReportDetail,
-        updatePostReport,
-    };
+  const report = async (reportee, type, content) => {
+    console.log(type);
+    return await axios.post(`/api/report/`, {
+      id: reportee,
+      type: type,
+      content: content,
+    });
+  };
+  const userReportList = async () => {
+    return await axios.get("/api/admin/report/userlist");
+  };
+  const userReportDetail = async (reportId) => {
+    return await axios.get(`/api/admin/report/userlist-detail/${reportId}`);
+  };
+  const updateUserReport = async (reportId, newStatus) => {
+    return await axios.put(
+      `/api/admin/report/user-status/${reportId}/status`,
+      null,
+      {
+        params: {
+          status: newStatus,
+        },
+      }
+    );
+  };
+  const postReportList = async () => {
+    return await axios.get("/api/admin/report/postlist");
+  };
+  const postReportDetail = async (reportId) => {
+    return await axios.get(`/api/admin/report/postlist-detail/${reportId}`);
+  };
+  const updatePostReport = async (reportId, newStatus) => {
+    return await axios.put(
+      `/api/admin/report/post-status/${reportId}/status`,
+      null,
+      {
+        params: {
+          status: newStatus,
+        },
+      }
+    );
+  };
+  return {
+    userReportList,
+    userReportDetail,
+    updateUserReport,
+    postReportList,
+    postReportDetail,
+    updatePostReport,
+    report,
+  };
 };
